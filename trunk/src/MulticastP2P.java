4b898d2ed6869450c95d97d3ea965d07c0b10a54
ricardo@ctrler.eu <ricardo@ctrler.eu@1e22db51-defd-c442-d2a4-792bb2a66f00>
2011-03-19 10:52:22 +0000

    Adicionei class SearchResult que servira para guardar resultados de pesquisa.
    Adicionei gerador de searchIDs

--- trunk/src/MulticastP2P.java
+++ trunk/src/MulticastP2P.java
@@ -5,13 +5,18 @@ import java.util.*;
 
 
 public class MulticastP2P {
-
+	
+	 public static final int MAXID = 9999999;
+	 public static final int CHUNKSIZE = 1024;
+	 
+	 Random randomGenerator; // stores the Random Number Generator
+	 
 	//MulticastSocket sockData; comented because it has to be createad every single thread for concurrent access
 	//MulticastSocket sockControl;
 	String currentSearchID; // Saves the current search id. We should not answer searches from ourselves.
 	InetSocketAddress controlAddr; // IP and Port for control
 	InetSocketAddress dataAddr; // IP and port for data
-	
+
 	private static Vector<fileStruct> fileArray = new Vector<fileStruct>();
 	
 	/**
@@ -25,7 +30,7 @@ public class MulticastP2P {
 		 * The control messages are always strings 
 		 * while the data messages are binary packets.  */
 		
-		// cha
+		// TODO gerar e guardar sha
 		String fileName;
 		long fileSize;
 		long totalChunks;
@@ -42,6 +47,25 @@ public class MulticastP2P {
 		}
 	}
 
+	
+	/**
+	 * Saves a search result
+	 */
+	private class SearchResult {
+		String searchID;
+		String sha;
+		long filesize;
+		String filename;
+		int peers; // Number of peers that have the file
+	}
+
+	
+	public MulticastP2P(){
+		
+		randomGenerator = new Random(); // Generates a random number sequence
+		
+	}
+	
 	
 	/**
 	 * @param args
@@ -74,12 +98,13 @@ public class MulticastP2P {
 		*/
 	}
 	
+
 	/**
 	 * Joins a multicast group and returns a Multicast socket
 	 * 
-	 * @param multicastAddress Multicast Group IP Address
-	 * @param port 
-	 * @throws IOException 
+	 * @param addr Multicast Group Address
+	 * @return socket to the multicast group
+	 * @throws IOException
 	 */
 	private MulticastSocket joinGroup(InetSocketAddress addr) throws IOException{
 
@@ -97,6 +122,13 @@ public class MulticastP2P {
 		
 	}
 	
+	private String genSearchID(){
+				
+		int number = randomGenerator.nextInt(MAXID);
+		return "id"  + number;
+		
+	}
+	
 	/**
 	 * Sends the searchString to the group and listens for results(?)
 	 * @param searchString
@@ -105,7 +137,7 @@ public class MulticastP2P {
 	 */
 	private void search(String keywordList) throws IOException{
 		
-		currentSearchID = "id12345679"; // TODO Gerar dinamicamente
+		currentSearchID = genSearchID();
 		
 		/* Create a new MulticastSocket so we can concurrently read and write
 		 * from the multicast group.
@@ -123,6 +155,7 @@ public class MulticastP2P {
 		
 		mSocket.send(searchPacket);
 		
+		// TODO While para guardar e apresentar resultados
 		byte[] buf = new byte[512];
 		DatagramPacket receivePacket = new DatagramPacket(buf,512);
 		mSocket.receive(receivePacket);
